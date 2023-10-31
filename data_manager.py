0c6691d953019fccd0b7ca3cc5a4933f03e0c443
Alican Çağdaş <alicancagdas1@hotmail.com>
2023-10-31 16:59:15 +0300

    updated data_manager

--- data_manager.py
+++ data_manager.py
@@ -12,5 +12,10 @@ class DataManager:
         y = self.df[["sales"]]
         return X, y
 
+    def getmulti_X_y(self):
+        X = self.df.drop("sales",axis=1)
+        y = self.df[["sales"]]
+        return X, y
+
     def get_data_description(self):
         return self.df.describe().T
